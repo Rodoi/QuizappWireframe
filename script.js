c7788a07bf8041ded5a212a92e6879f726d4ecb4
Rodoi <52259381+Rodoi@users.noreply.github.com>
2019-09-24 11:54:21 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+/*
+what is the capital city of Uganda?
+a.Kawempe
+b.Kampala
+c.kamwenge
+d.kamwokya 
+
+CorrectAnswer:"Kampala" 
+
+Alt "Uganda_flag.png"
+
+What is the Capital City of Mauritius?
+a. Quatre Bornes 	
+b. Port Louis 		
+c. Boumerdas 		
+d. Nouakchott 
+
+CorrectAnswer: "Port Louis"
+
+
+Alt:"Mauritius-flag.png"
+
+What is the Capital City of Chad?
+a. NDema 		
+b. Ziguinchor 		
+c. NDjedaa		
+d. NDjamena 
+
+CorrectAnswer: "NDjamena"
+
+alt: "Chad_flag.png"
+
+What is the Capital City of Djibouti?
+a. Addis Ababa 	
+b. Asmara 		
+c. Djibouti 		
+d. NDjedid 
+
+CorrectAnswer: "Djibouti"
+
+alt: "Djibouti_flag.png"
+
+What is the Capital City of Mali?
+a. Marouane	 	
+b. Lagos 		
+c. Bamako		
+d. Timbuktu  
+
+CorrectAnswer: "Bamako"
+
+alt: "Mali_flag.png"
+
+What is the Capital City of Guinea-Bissau?
+a. Niokolo-Koba 	
+b. Bissau 		
+c. Conakry 		
+d. Bissalanca 
+
+CorrectAnswer: "Bissau"
+
+alt: "Guinea-Bissau_flag.png"
+
+What is the Capital City of Central Africa Republic?
+a. Bambari 		
+b. Bangui 		
+c. Bakouma 		
+d. Shinyanga  
+
+CorrectAnswer: "Bangui"
+
+alt: "Central Africa Rep_flag.png"
+
+What is the Capital City of Rwanda?
+a. Kigali 	
+b. Gisenyi 		
+c. Butare 		
+d. Nyagatare
+
+CorrectAnswer: "Kigali"
+
+alt: "Rwanda_flag.png"
+
+What is the Capital City of Comoros? 
+a. Mutumba 		
+b. Bujumbura 		
+c. Domoni 		
+d. Moroni  
+
+CorrectAnswer: "Moroni"
+
+alt: "Comoros_flag.png"
+
+What is the Capital City of Seychelles? 
+a. Mare Anglaise 	
+b. Pointe Au Sel	
+c. Victoria 		
+d. Anse Royale  
+
+CorrectAnswer: "Victoria"
+
+alt: "Seychelles_flag.png"
+
+*/
